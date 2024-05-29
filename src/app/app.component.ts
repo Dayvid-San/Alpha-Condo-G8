fd4fc73c57c739888ec0c4b3ab236b69b04b6ca1
Dayvid Santana <dayvidcomerce@gmail.com>
2024-05-29 17:18:11 -0300

    feat: header and button components added

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./ui/header/header.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [
+    RouterOutlet,
+    HeaderComponent
+  ]
+})
+export class AppComponent {
+  title = 'alpha-condo-G8';
+}
