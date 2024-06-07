ea97d1488dbe5d33fb7dc6c7a08fbfe10429c5bf
Dayvid Santana <dayvidcomerce@gmail.com>
2024-06-07 01:14:51 -0300

    feat: forum component added

--- src/app/accounts/accounts.routes.ts
+++ src/app/accounts/accounts.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { ResidentComponent } from './features/resident/resident.component';
+import { ForumComponent } from './features/forum/forum.component';
 
 
 export const routes: Routes = [
-    {
-        path: 'residentes',
-        component: ResidentComponent,
-      },
+  {
+    path: 'residentes',
+    component: ResidentComponent,
+  },
+  {
+    path: 'forum',
+    component: ForumComponent,
+  }
 ];
