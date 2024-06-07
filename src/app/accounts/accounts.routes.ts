f023faf614ad8b7d6620ceb94fd0744a32e99566
Dayvid Santana <dayvidcomerce@gmail.com>
2024-06-07 01:16:52 -0300

    feat: projects page component added

--- src/app/accounts/accounts.routes.ts
+++ src/app/accounts/accounts.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ResidentComponent } from './features/resident/resident.component';
 import { ForumComponent } from './features/forum/forum.component';
+import { ProjectsComponent } from './features/projects/projects.component';
 
 
 export const routes: Routes = [
@@ -11,5 +12,9 @@ export const routes: Routes = [
   {
     path: 'forum',
     component: ForumComponent,
+  },
+  {
+    path: 'projetos',
+    component: ProjectsComponent,
   }
 ];
