a4661081bdee0b1785ca7fe72a642e71d8ea8c8f
Dayvid Santana <dayvidcomerce@gmail.com>
2024-06-07 23:54:07 -0300

    feat: not found component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 
 import { routes as accountRoutes } from './accounts/accounts.routes';
+import { NotFoundComponent } from './core/ui/not-found/not-found.component';
 
 
 export const routes: Routes = [
     ...accountRoutes,
     { path: '', pathMatch: 'full', redirectTo: 'entrar' },
+    { path: '**', pathMatch: 'full', component: NotFoundComponent },
 ];
